3f0c5da9f7fac15ce01a76efa50d1b6fda01924c
TwerkingPanda <75905616+TwerkingPanda@users.noreply.github.com>
2020-12-18 19:51:07 +0530

    Update jpg2png.py

--- jpg2png.py
+++ jpg2png.py
@@ -2,15 +2,20 @@ from PIL import Image
 import os
 import sys
 import pdb
-input_path  = input("Enter the full path to the folder with JPEG files or place the program file with the image folder")
-output_path = input("Enter the full path to the folder where the converted images are to be stored")
+input_path  = input("Enter the full path to the folder with JPEG files or place the program file with the image folder:\n")
+output_path = 
 a=os.listdir(input_path)
 os.chdir(input_path)
+output_path = "/PNGimages"
 try:
 	os.mkdir(output_path)
 except:
-	print("Failed to create the output directory. Make sure that the program has suffiecient rights/ try with another folder")
+	print("Failed to create the output directory. Most likely, you do not have access to create directories at this location")
+	try:
+		output_path = input("Enter full path of where you want to save your converted images:\n")
+	except:
+		print("Failed to create the output directory. Most likely, you do not have access to create directories at this location")
 for i in a:
-	if i[:-4] == '.jpg' :
+	if i[-4:] == '.jpg' :
 		img = Image.open(i)	
 		img.save(f'{output_path}/{i[:-4]}'+'.png')
